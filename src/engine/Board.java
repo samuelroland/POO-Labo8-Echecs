bae9d386d474731a869662f9514518f0cb07a339
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-01-04 14:38:15 +0100

    Add Board.lookIfKingsInCheck()

--- src/engine/Board.java
+++ src/engine/Board.java
@@ -2,10 +2,14 @@ package engine;
 
 import java.security.InvalidParameterException;
 
+import chess.PieceType;
+import chess.PlayerColor;
 import engine.pieces.Piece;
 
 public class Board {
 	private Piece[][] pieces;
+	private boolean blackKingInCheck = false;
+	private boolean whiteKingInCheck = false;
 
 	Board(Piece[][] pieces) {
 		if (pieces.length != 8)
@@ -22,6 +26,58 @@ public class Board {
 	void movePieces(Point from, Point to) {
 		// params okay ??
 		// how can we do multiple pieces moves ?
+	}
+
+	// Est-ce qu'un des 2 rois sont en échecs, sur le plateau temporaire fourni ?
+	// Utile pour savoir s'il peut y sauter ou que c'est déjà le cas
+	// si c'est une autre pièce qui bouge
+	public void lookIfKingsInCheck(Piece[][] piecesCopy) {
+		// On cherche d'abord la position des 2 rois
+		// TODO: devrait-on plutot stocker les positisions des 2 rois ??
+		Point blackKingPos = null, whiteKingPos = null;
+		for (int i = 0; i < piecesCopy.length; i++) {
+			for (int j = 0; j < piecesCopy[i].length; j++) {
+				if (isEmpty(i, j))
+					continue;
+				Piece p = piecesCopy[i][j];
+				if (p.getType().equals(PieceType.KING)) {
+					if (p.getColor().equals(PlayerColor.BLACK)) {
+						blackKingPos = p.getPoint();
+					} else {
+						whiteKingPos = p.getPoint();
+					}
+				}
+			}
+			// On a trouvé les 2 rois, on peut s'arrêter de chercher
+			if (blackKingPos != null && whiteKingPos != null) {
+				break;
+			}
+		}
+
+		// Note: On est garanti d'avoir les positions des 2 rois, les pièces ne peuvent
+		// jamais être retirées du plateau
+
+		// On check pour toutes les pièces est-ce que d'aller sur cette position est un
+		// coup valide, si oui, alors une pièce menace le roi de l'autre couleur
+		for (int i = 0; i < piecesCopy.length; i++) {
+			for (int j = 0; j < piecesCopy[i].length; j++) {
+				if (isEmpty(i, j))
+					continue;
+				Piece p = getPiece(i, j);
+
+				if (p.getColor().equals(PlayerColor.WHITE)) {
+					// Est-ce que cette pièce blanche menace le roi noir ?
+					if (p.isValid(blackKingPos)) {
+						blackKingInCheck = true;
+					}
+				} else {
+					// Est-ce que cette pièce noire menace le roi blanc ?
+					if (p.isValid(whiteKingPos)) {
+						whiteKingInCheck = true;
+					}
+				}
+			}
+		}
 	}
 
 	void addPiece(Piece piece) {
